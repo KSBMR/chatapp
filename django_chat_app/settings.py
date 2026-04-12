ea68788f955bc9eab1ee14b592e84ace5d086575
KSBMR <shahbazrafi2020@gmail.com>
2026-04-12 16:58:25 +0600

    Configure for Render and Neon

--- django_chat_app/settings.py
+++ django_chat_app/settings.py
@@ -1,7 +1,7 @@
 """
 Django settings for django_chat_app project.
 """
-
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,14 +10,14 @@ SECRET_KEY = 'django-insecure-change-this-in-production-use-env-variable'
 
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 DEBUG = False
-ALLOWED_HOSTS = [
-    'chatingapp.pythonanywhere.com',  # Your new live site
-    'KSBMR.pythonanywhere.com',       # Your old site (if still active)
-    '127.0.0.1',                      # Localhost (for your PC)
-    'localhost',                      # Localhost (for your PC)
-]
+# ALLOWED_HOSTS = [
+#     # 'chatingapp.pythonanywhere.com',  # Your new live site
+#     # 'KSBMR.pythonanywhere.com',       # Your old site (if still active)
+#     '127.0.0.1',                      # Localhost (for your PC)
+#     'localhost',                      # Localhost (for your PC)
+# ]
 
 # Application definition
 INSTALLED_APPS = [
@@ -77,13 +77,20 @@ CHANNEL_LAYERS = {
     },
 }
 
-# Database
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
+# Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
